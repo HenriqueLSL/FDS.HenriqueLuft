e6d5566d429d61bad121dbc47a9dc97ad6f58342
HenriqueLSL <Lift063@gmail.com>
2025-07-06 17:42:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ function mostrarMensagem() {
+    const mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.textContent = "Desafios existem para te mostrar do que você é capaz, a persistência é uma das chaves para o sucesso!!!";
+  }
